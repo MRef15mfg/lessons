503946cced3f79d1412a346b93372ccc83e829ab
MRef15mfg <mrefmfg@gmail.com>
2025-06-07 10:25:35 +0200

    Update lesson1_2.py

--- lesson1_2.py
+++ lesson1_2.py
@@ -10,3 +10,9 @@ print(f"Площа прямокутника {w} та {h} = {float(w) * float(h)}
 
 radius = float(input("radius : "))
 print(f"Площа кола з радіусом {radius} = {math.pi * radius ** 2} см^2")
+
+
+c = float(input("Введи температуру в Цельсіях: "))
+f = c * 9 / 5 + 32
+print("Температура в Фаренгейтах:", f)
+
