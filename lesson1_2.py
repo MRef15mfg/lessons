f38b838316bb164d282167f162dda209fafa1700
MRef15mfg <mrefmfg@gmail.com>
2025-06-07 10:15:08 +0200

    Create lesson1_2.py

---
+++ lesson1_2.py
@@ -0,0 +1,12 @@
+import math
+name = input("Enter yout name : ")
+print(f"Hello {name}!")
+
+
+w = input("Enter weight : ")
+h = input("Enter height : ")
+
+print(f"Площа прямокутника {w} та {h} = {float(w) * float(h)} см^2")
+
+radius = float(input("radius : "))
+print(f"Площа кола з радіусом {radius} = {math.pi * radius ** 2} см^2")
